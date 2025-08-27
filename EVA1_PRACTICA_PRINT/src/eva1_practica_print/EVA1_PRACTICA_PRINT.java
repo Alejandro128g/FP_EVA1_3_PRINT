70f0cf14d82c89f2d99b767930621e02a3305139
Alejandro128g <alejandrojacquez123@gmail.com>
2025-08-26 23:28:41 -0600

    Add files via upload

---
+++ EVA1_PRACTICA_PRINT/src/eva1_practica_print/EVA1_PRACTICA_PRINT.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practica_print;
+
+/**
+ *
+ * @author ALEJANDRO JAQUEZ
+ */
+public class EVA1_PRACTICA_PRINT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Nombre del estudiante");
+        System.out.print("Jeseus Alejandro Jacquez Olivas");
+        System.out.println("NO. CONTROL");
+        System.out.print("25550427");
+        System.out.println("EDAD");
+        System.out.print("18");
+         
+    }
+    
+}
